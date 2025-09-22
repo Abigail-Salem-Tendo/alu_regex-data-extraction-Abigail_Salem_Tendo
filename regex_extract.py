8c05ac0e8fbd513df7630fb5f3bfc2d52381c20c
Abigail Salem Tendo <salemabigail10@gmail.com>
2025-09-22 10:09:06 +0200

    Description of the project

---
+++ regex_extract.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""
+Regex Data Extraction Project
+Description: This is a simple project on how I will extract information like
+emails, urls, time, hashtags and phone numbers from a sample text.
+"""
